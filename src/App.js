61f804c1143443069ca010f40fbd64c74bb76f75
japankid-code <japankid.jake@gmail.com>
2021-08-07 10:09:47 -0500

    update for deploy

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/projects" component={Projects} />
-          <Route exact path="/resume" component={Resume} />
+          <Route exact path="/portfolio/" component={About} />
+          <Route exact path="/portfolio/about" component={About} />
+          <Route exact path="/portfolio/projects" component={Projects} />
+          <Route exact path="/portfolio/resume" component={Resume} />
         </Switch>
         <Footer></Footer>
       </Router>
