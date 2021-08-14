c8e3d022b2d57747454c02491fb7efec5c7e8772
japankid-code <japankid.jake@gmail.com>
2021-08-13 21:42:22 -0500

    refactor projects data

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,55 +3,9 @@ import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 
-import dingo0 from "../assets/images/dingo/0.png";
-import dingo1 from "../assets/images/dingo/1.png";
-import dingo2 from "../assets/images/dingo/2.png";
-import dingo3 from "../assets/images/dingo/3.png";
-import dingo4 from "../assets/images/dingo/4.png";
-import dingo5 from "../assets/images/dingo/4.png";
-import dingo6 from "../assets/images/dingo/4.png";
-import drawWithFrendz0 from "../assets/images/draw-with-friends/0.png";
-import drawWithFrendz1 from "../assets/images/draw-with-friends/1.png";
-import drawWithFrendz2 from "../assets/images/draw-with-friends/2.png";
-import theWineCage0 from "../assets/images/the-wine-cage/0.png";
-import theWineCage1 from "../assets/images/the-wine-cage/1.png";
-import theWineCage2 from "../assets/images/the-wine-cage/2.png";
-import weatherKid0 from "../assets/images/weather-kid/0.png";
-import weatherKid1 from "../assets/images/weather-kid/1.png";
-import weatherKid2 from "../assets/images/weather-kid/2.png";
+import { projects } from "../utils/projectData";
 
 function Projects() {
-  const projects = [
-    {
-      name: "Dingo",
-      deploy_link: "https://dingo-dog-walker.herokuapp.com/",
-      repo_link: "https://github.com/bc-mern-stack/dingo",
-      images: [dingo0, dingo1, dingo2, dingo3, dingo4, dingo5, dingo6],
-      gist: "MERN stack React App",
-    },
-    {
-      name: "Draw With Friends",
-      deploy_link: "https://draw-with-friendz.herokuapp.com/",
-      repo_link: "https://github.com/japankid-code/draw-stuff",
-      images: [drawWithFrendz0, drawWithFrendz1, drawWithFrendz2],
-      gist: "Complete MVC Application",
-    },
-    {
-      name: "The Wine Cage",
-      deploy_link: "https://bootcamp-project1.github.io/The-wine-cage/",
-      repo_link: "https://github.com/bootcamp-project1/The-wine-cage",
-      images: [theWineCage0, theWineCage1, theWineCage2],
-      gist: "Collaborative server side API project",
-    },
-    {
-      name: "Weather Kid",
-      deploy_link: "https://japankid-code.github.io/weather-kid/",
-      repo_link: "https://github.com/japankid-code/weather-kid",
-      images: [weatherKid0, weatherKid1, weatherKid2],
-      gist: "vanilla JS app",
-    },
-  ];
-
   function carousel(images) {
     return (
       <Carousel
